90efd11adaf0003a699418086e1d711691903257
JuchokJuk <juk.juchok@yandex.ru>
2024-02-15 12:35:57 +0300

    .

--- src/components/Inputs/SearchInput/searchStatisticsAPI.js
+++ src/components/Inputs/SearchInput/searchStatisticsAPI.js
@@ -1,33 +1,34 @@
 const service = process.env.NEXT_PUBLIC_SEARCH_STATISTICS_API_URL;
 
 export async function getNewId() {
-  const url = `${service}getNewId`;
+  // const url = `${service}getNewId`;
 
-  const response = await fetch(url, {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "GET",
-  });
+  // const response = await fetch(url, {
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   method: "GET",
+  // });
 
-  const responseBody = await response.json();
-  const newId = responseBody.newId;
-  return newId;
+  // const responseBody = await response.json();
+  // const newId = responseBody.newId;
+  // return newId;
+  return 0;
 }
 
 export function sendRequest(id, request) {
-  const url = `${service}sendRequest`;
+  // const url = `${service}sendRequest`;
 
-  fetch(url, {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({
-      id: id,
-      request: request,
-    }),
-  });
+  // fetch(url, {
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     id: id,
+  //     request: request,
+  //   }),
+  // });
 }
